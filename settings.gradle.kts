7d52f6084076103b7ad5cbac690a3575c2d5ffb5
Marchael Acode <m.acode@outlook.com>
2022-07-14 23:36:15 -0700

    Adding ktor plugin files.

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "com.github.kuya32.ktor-vintracker"
---
+++ src/main/kotlin/com/github/kuya32/Application.kt
@@ -0,0 +1,17 @@
+package com.github.kuya32
+
+import io.ktor.server.application.*
+import com.github.kuya32.plugins.*
+
+fun main(args: Array<String>): Unit =
+    io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
+fun Application.module() {
+    configureRouting()
+    configureSockets()
+    configureSerialization()
+    configureMonitoring()
+    configureHTTP()
+    configureSecurity()
+}
